173a680990d892ff17b3aec45818669e35f39181
petejos80 <petejos80@gmail.com>
2020-05-03 15:24:48 -0500

    Formatting posts content

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,39 +1,54 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {
+  Section,
+  Container,
+  Title,
+  Subtitle,
+  Button,
+  Icon,
+  Column,
+  Columns,
+  Box,
+} from "bloomer"
+
 
 const Posts = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <ul style={{ listStyle: "none" }}>
-      {data.allWordpressPost.edges.map(post => (
-        <li style={{ padding: "20px 0", borderBottom: "1px solid #ccc" }}>
-          <Link
-            to={`/post/${post.node.slug}`}
-            style={{ display: "flex", color: "black", textDecoration: "none" }}
-          >
-            <Img
-              sizes={post.node.featured_media.localFile.childImageSharp.sizes}
-              alt={post.node.title}
-              style={{ width: "25%", marginRight: 20 }}
-            />
-            <div style={{ width: "75%" }}>
-              <h3
-                dangerouslySetInnerHTML={{ __html: post.node.title }}
-                style={{ marginBottom: 0 }}
-              />
-              <p style={{ margin: 0, color: "grey" }}>
-                Written by {post.node.author.name} on {post.node.date}
-              </p>
-              <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <Section>
+      <Container>
+        <SEO title="Posts" keywords={[`gatsby`, `application`, `react`]} />
+        <ul style={{ listStyle: "none" }}>
+          {data.allWordpressPost.edges.map(post => (
+            <li style={{ padding: "20px 0", borderBottom: "1px solid #ccc" }}>
+              <Link
+                to={`/post/${post.node.slug}`}
+                style={{ display: "flex", color: "black", textDecoration: "none" }}
+              >
+                <Img
+                  sizes={post.node.featured_media.localFile.childImageSharp.sizes}
+                  alt={post.node.title}
+                  style={{ width: "25%", marginRight: 20 }}
+                />
+                <div style={{ width: "75%" }}>
+                  <h3
+                    dangerouslySetInnerHTML={{ __html: post.node.title }}
+                    style={{ marginBottom: 0 }}
+                  />
+                  <p style={{ margin: 0, color: "grey" }}>
+                    Written by {post.node.author.name} on {post.node.date}
+                  </p>
+                  <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </Section>
   </Layout>
 )
 
