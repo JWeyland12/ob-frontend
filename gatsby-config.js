bd945ac6deed5824b660297e310f7abc80230ebe
petejos80 <petejos80@gmail.com>
2020-06-15 21:11:22 -0500

    Added functionality for generating comment links.  Link a tag and scroll functionality still needs to be refined

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,12 @@ module.exports = {
           return entities
         },
       },
-      
+    },
+    {
+      resolve: "gatsby-plugin-anchor-links",
+      options: {
+        offset: -100
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,4 +7,4 @@
 // You can delete this file if you're not using it
 
 // Apollo client
-export { wrapRootElement } from './src/apollo/wrap-root-element';
+// export { wrapRootElement } from './src/apollo/wrap-root-element';
