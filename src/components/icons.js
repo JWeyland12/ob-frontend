29e8eb15c231c86051f3c9200a5d11b150c51006
petejos80 <petejos80@gmail.com>
2020-08-13 22:55:22 -0500

    Added formatting for featured posts on homepage, and added formatting for social icons

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,40 +1,49 @@
 import React from "react"
 import { Icon } from "bloomer"
 
-const SocialIcons = () => (
-  [
+const SocialIcons = () => {
+  
+  const data = [
     {
       // Icon color #ff0000
       "url":"https://www.google.com",
-      "icon": "fab fa-youtube fa-lg youtube",
+      "icon": "fab fa-youtube youtube fa-lg",
     },
     {
       // Icon color #3b5998
       "url":"https://www.google.com",
-      "icon":"fab fa-facebook fa-lg facebook",
+      "icon":"fab fa-facebook facebook fa-lg",
     },
     {
       // Icon color #b31217
       "url":"https://www.google.com",
-      "icon":"fab fa-pinterest fa-lg pinterest",
+      "icon":"fab fa-pinterest pinterest fa-lg",
     },
     {
       // Icon color #f10176
       "url":"https://www.google.com",
-      "icon":"fab fa-instagram fa-lg instagram",
+      "icon":"fab fa-instagram instagram fa-lg",
     },
     {
       // Icon color #55acee
       "url":"https://www.google.com",
-      "icon":"fab fa-twitter fa-lg twitter",
+      "icon":"fab fa-twitter twitter fa-lg",
     },
-  ].map(function(d, idx){
+  ];
+  
+  const icons = data.map(function(d, idx){
     return (
-      <a className="post-icons" href={d.url} key={idx}>
+      <a className="icon-row" href={d.url} key={idx}>
         <Icon className={d.icon}/>
       </a>
-    )
-  })
-)
+    )}
+  );
+
+  return (
+    <div className={"icon-container"}>
+      {icons}
+    </div>
+  ) 
+}
 
 export default SocialIcons
